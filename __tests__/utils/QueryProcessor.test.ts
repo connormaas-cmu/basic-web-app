928021458661945ab83db263f99f0267d8ac92c7
connormaas-cmu <cmaas@andrew.cmu.edu>
2023-09-15 14:30:29 -0400

    Implemented what is your andrew ID

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -17,4 +17,12 @@ describe("QueryProcessor", () => {
             "writer in the English language and the world's pre-eminent dramatist."
           ));
     });
+
+    test('should return andrew ID message', () =>  {
+        const query = "andrewid";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "My andrew ID is cmaas."
+        )); 
+    })
 });
